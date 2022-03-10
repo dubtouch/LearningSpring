e91faf9fc33fec33e895ccbac24bcd31bec7440d
dubtouch <postolachedan92@gmail.com>
2022-03-10 13:58:09 +0200

    edit documentation

--- rest-demo/src/main/java/dub/dubtouch/restdemo/SwaggerConfig.java
+++ rest-demo/src/main/java/dub/dubtouch/restdemo/SwaggerConfig.java
@@ -3,6 +3,8 @@ package dub.dubtouch.restdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,8 +14,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+            "Demo API Title",
+            "Demo API Documentation",
+            "1.0",
+            "urn:tos",
+            "demo contact",
+            "Apache 2.0",
+            "https://www.apache.org/licenses/LICENSE-2.0"
+    );
+
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO);
     }
 }
