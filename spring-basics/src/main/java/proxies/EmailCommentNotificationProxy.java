31d439a83c8afd306cd65473fb162162ba65b346
dubtouch <postolachedan92@gmail.com>
2022-03-13 18:19:51 +0200

    qualifiers

--- spring-basics/src/main/java/proxies/EmailCommentNotificationProxy.java
+++ spring-basics/src/main/java/proxies/EmailCommentNotificationProxy.java
@@ -1,13 +1,15 @@
 package proxies;
 
 import model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Qualifier("EMAIL")
 public class EmailCommentNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending notification for comment: " + comment.getText());
+        System.out.println("Sending email notification for comment: " + comment.getText());
     }
 }
--- spring-basics/src/main/java/services/CommentService.java
+++ spring-basics/src/main/java/services/CommentService.java
@@ -2,16 +2,18 @@ package services;
 
 import model.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import proxies.CommentNotificationProxy;
 import repositories.CommentRepository;
-@Component
+@Service
 public class CommentService {
     private final CommentNotificationProxy commentNotificationProxy;
     private final CommentRepository commentRepository;
 
     @Autowired
-    public CommentService(CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
+    public CommentService(@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, CommentRepository commentRepository) {
         this.commentNotificationProxy = commentNotificationProxy;
         this.commentRepository = commentRepository;
     }
