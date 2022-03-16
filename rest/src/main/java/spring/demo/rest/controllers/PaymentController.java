fe67be23359ff408e821a799698d93496ce5110a
dubtouch <postolachedan92@gmail.com>
2022-03-16 16:43:41 +0200

    manage exceptions, rest controller advice

--- rest/src/main/java/spring/demo/rest/controllers/PaymentController.java
+++ rest/src/main/java/spring/demo/rest/controllers/PaymentController.java
@@ -3,24 +3,23 @@ package spring.demo.rest.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import spring.demo.rest.exceptions.NotEnoughMoneyException;
 import spring.demo.rest.models.ErrorDetails;
 import spring.demo.rest.models.PaymentDetails;
 import spring.demo.rest.services.PaymentService;
 
+import java.util.logging.Logger;
+
 @RestController
 public class PaymentController {
 
-    private final PaymentService paymentService;
-
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info("Received amount " + paymentDetails.getAmount());
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(paymentDetails);
     }
 }
