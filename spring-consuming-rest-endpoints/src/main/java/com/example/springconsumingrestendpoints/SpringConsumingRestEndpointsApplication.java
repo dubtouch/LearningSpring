92645dbcb5a62ce4452e58ea0d07eb5ecd08ee43
dubtouch <postolachedan92@gmail.com>
2022-03-16 17:50:06 +0200

    openfeign implementation

---
+++ spring-consuming-rest-endpoints/src/main/java/com/example/springconsumingrestendpoints/SpringConsumingRestEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springconsumingrestendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsumingRestEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumingRestEndpointsApplication.class, args);
+	}
+
+}
